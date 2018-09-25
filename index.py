e1b7546a0d94fe5c15997c0504b928da19e31b49
Matthew Brown <mattd429@gmail.com>
2018-09-25 18:45:50 -0400

    changing project structure around to move away from any tutorial creating custom project

---
+++ index.py
@@ -0,0 +1,14 @@
+""" index file for REST APIs using Flask """
+import os
+import sys
+import requests
+from flask import jsonify, request, make_response, send_from_directory
+
+""" Join Root path with `flaskr` path using """
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+os.environ.update({'ROOT_PATH': ROOT_PATH})
+sys.path.append(os.path.join(ROOT_PATH, 'flaskr'))
+
+
+import logger
+from  import app
