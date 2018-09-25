829eb628f17cead4abdeb7c977591bb19f29429f
Matthew Brown <mattd429@gmail.com>
2018-09-25 18:50:57 -0400

    another commit had to change the index.py appending the joining of root with modules path.

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ from flask import jsonify, request, make_response, send_from_directory
 """ Join Root path with `flaskr` path using """
 ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
 os.environ.update({'ROOT_PATH': ROOT_PATH})
-sys.path.append(os.path.join(ROOT_PATH, 'flaskr'))
+sys.path.append(os.path.join(ROOT_PATH, 'modules'))
 
 
 import logger
