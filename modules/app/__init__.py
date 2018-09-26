cd4e9cf6e39eda2483f608851f774c8e6af9a8fd
Matthew Brown <mattd429@gmail.com>
2018-09-26 17:52:44 -0400

    removed old schema we are setting up using PyMongo instead of sql, added a JSONEncoder to our app __init__.py file to return ObjectId and datetime into strings, created the connection for our flask_PyMongo, the Flask config can now create the connection(not fully down yet!) -> last we set the default json-encoder to our customer class json encoder. -> added more packages to requires.txt

--- modules/app/__init__.py
+++ modules/app/__init__.py
@@ -1,8 +1,30 @@
+# Python imports
 import os
 import json
 import datetime
-
+# PIP imports
+from bson.objectid import ObjectId
 from flask import Flask
+from flask_pymongo import PyMongo
+# Local imports
+#from app.controllers import *
+
+class JSONEncoder(json.JSONEncoder):
+
+    def default(self, o):
+        """
+        :Extend the json-encoder class to support `ObjectId` & `datetime`
+        data types used to store `_id` & `time-stamp` as a group in MongoDB.
+        All the responses need to be converted into json string, to enable the cross-platform
+        data interpretation.  We convert the `ObjectId` & `datetime` to a `string`.
+        :param o: calling to the object
+        :return: in string format.
+        """
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
 
 from sentry_sdk import configure_scope
 with configure_scope() as scope:
@@ -29,6 +51,13 @@ def create_app(test_config=None):
     """
     app = Flask(__name__, instance_relative_config=True)
     sentry.init_app(app)
+
+    ''' Add mongo url to flask config, this allows flask_pymongo to make the connection. 
+        The mongo object returns can be used in all the routes
+    '''
+    app.config['MONGO_URI'] = os.environ.get('DB')
+    mongo = PyMongo(app)
+
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -50,4 +79,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    ''' modify the default json-encoder with our customer json class extender. '''
+    app.json_encoder = JSONEncoder
+
     return app
+
