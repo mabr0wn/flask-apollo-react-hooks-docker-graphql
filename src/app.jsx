6cd47e5b80636e11642fa9775e85f1e3255229e2
Matthew Brown <mattd429@gmail.com>
2018-09-26 20:11:28 -0400

    ++ yarn  => package management.

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,19 @@
-import * as  React from 'react';
-import * as ReactDom from 'react-dom';
+import * as React from 'react';
+import { Component } from 'react';
 
-ReactDOM.render(
-    <app />, document.getElementById('root'));
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Flask + React + Docker + AWS</h2>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import * as  React from 'react';
+import * as ReactDom from 'react-dom';
+import App from './app';
+
+const jsx = (
+        <app />
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
