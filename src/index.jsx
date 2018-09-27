06f91964b60fc6b9f5ca231f60f04042f29e125c
Matthew Brown <mattd429@gmail.com>
2018-09-27 10:01:40 -0400

    added babel config file, => react will talk to DOM, added -D dependencies.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,17 @@
-import * as  React from 'react';
-import * as ReactDom from 'react-dom';
-import App from './app';
+// import * as  React from 'react';
+// import * as ReactDom from 'react-dom';
+// import App from './app';
 
-const jsx = (
-        <app />
+// const jsx = (
+//         <app />
+// );
+
+// ReactDOM.render(jsx, document.getElementById('app'));
+
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  React.createElement("div", null, "Hello World"), // <div>Hello World</div>
+  document.getElementById("root")
 );
-
-ReactDOM.render(jsx, document.getElementById('app'));
