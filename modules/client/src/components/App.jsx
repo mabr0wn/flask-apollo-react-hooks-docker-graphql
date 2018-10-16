79ab5d4b53b38154857795dd24d2d2b15ccb5a8d
Matt Brown <mattd429@gmail.com>
2018-10-16 13:45:36 -0400

    added redux, react-redux, created container and presentational component boilerplate for setting up redux to mutate our blogs, reducers and actions.

--- modules/client/src/components/App.jsx
+++ modules/client/src/components/App.jsx
@@ -1,26 +1,28 @@
 // React
 import * as React from 'react';
 import { Component } from 'react';
+// Local
+import Blogs  from '../containers/Blogs';
 
 
 
 class App extends Component {
-  // eslint-disable-next-line
-  constructor() { 
-      super();
-
-    }
     /*
      * The `render()` function should be pure, meaning that it 
      * does not modify component state, it returns the same 
      * result each time it’s invoked, and it does not directly 
      * interact with the browser.  
+     * =======================================================
+     * Presentational component, will not worry about
+     * how the applications works, but only care about how
+     * our application will looks.
      */ 
     render() {
         return (
             <div className="App">
                 <div className="App-header">
                     <h2>Flask + React + Docker + GraphQL</h2>
+                    <Blogs />
                 </div>
             </div>
         );
