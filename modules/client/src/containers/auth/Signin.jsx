cf619450506dae4475d032780ada9b1ea8674c62
Matt Brown <mattd429@gmail.com>
2018-10-29 11:07:50 -0400

    moved around folders add redux-form

---
+++ modules/client/src/containers/auth/Signin.jsx
@@ -0,0 +1,16 @@
+// React
+import React, { Component } from 'react'
+import { reduxForm } from 'redux-form';
+
+
+class Signin extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Signin;
