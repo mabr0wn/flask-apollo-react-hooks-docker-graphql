db04a986e742e8685d6d1a07ca035bcb51a7804f
Matt Brown <mattd429@gmail.com>
2018-10-29 12:50:47 -0400

    started adding the reducers and actions for redux pattern a little messy as the moment but will clean up shortly.

--- modules/client/src/components/App.jsx
+++ modules/client/src/components/App.jsx
@@ -3,8 +3,27 @@ import * as React from 'react';
 import { Component } from 'react';
 // Local
 import Blogs  from '../containers/Blogs';
+import Signin from '../containers/auth/Signin';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+/* import promise from 'redux-promise';*/
+import reduxThunk from 'redux-thunk';
+import { AUTH_USER } from '../actions/types';
+import reducers from '../reducers';
 
+// Connect reduxThunk to middleware so I could dispatch actions.
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
+// store contains the state
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+// if user has a token - sign him in
+if (token) {
+    store.dispatch({ type: AUTH_USER });
+    /* console.log(">>>> src/index.js:");	    
+     * console.log("localStorage contains token, so sign user in.");    */
+}
 
 class App extends Component {
     /*
@@ -19,12 +38,15 @@ class App extends Component {
      */ 
     render() {
         return (
+            <Provider store={store}>
             <div className="App">
                 <div className="App-header">
                     <h2>Flask + React + Docker + GraphQL</h2>
                     <Blogs />
+                    <Signin />
                 </div>
             </div>
+            </Provider>
         );
     }
 }
