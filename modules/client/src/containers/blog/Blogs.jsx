b6ca1be7be137971669f0e662100909fd308a3a3
Matt Brown <mattd429@gmail.com>
2018-11-05 17:13:58 -0500

    converted into a React Function Component for Blogs.jsx

--- modules/client/src/containers/blog/Blogs.jsx
+++ modules/client/src/containers/blog/Blogs.jsx
@@ -1,7 +1,5 @@
 // React
-import React, { 
-    Component 
-} from 'react';
+import React from 'react';
 import { 
     connect 
 } from 'react-redux';
@@ -15,61 +13,56 @@ import Blog from './Blog.jsx';
  * This is container class which will do all the heavy lifting,
  * will import this into our presentational component App.js
  */
-class Blogs extends Component {
 
-    blogsData() {
-        const blogs = this.props.blogs.results;
-        /**
-         * If there are no blogs in the state
-         * render an empty div tag
-         */
-        if (!blogs) {
-            return (
-                <div>
-                </div>
-            );
-        };
-
-        return blogs.map((blog) => {
-            if (blog.published || this.props.authenticated) {
-                /** 
-                 * Generate the list of blogs. 
-                 * Published blogs ar visiable to all
-                 * authenticated user can see both published and drafts
-                 */
-                return (
-                    <Blog key={blog.slug}
-                          title={blog.title}
-                          slug={blog.slug}
-                          body={blog.body}
-                          published={blog.published}
-                          authenticated={this.props.authenticated}
-                          category={blog.category}
-                          tags={blog.tags}
-                          cut={100}
-                          link={`/blog/${blog.slug}`}/>
-                )
-            } else {
-                return (
-                    <Blog key={blog.slug}
-                    title={blog.title}
-                    slug={blog.slug}
-                    body={blog.body}
-                    category={blog.category}
-                    tags={blog.tags}
-                    link={`/blog/${blog.slug}`}/>
-                );
-            }
-        });
-    }
-
-    render() {
+const blogsData = (blogs, props) => {
+    /**
+     * If there are no blogs in the state
+     * render an empty div tag
+     */
+    if (!blogs) {
         return (
-            <div>
-		        { this.blogsData() }
-            </div>
+           <div></div>
         );
-    }
+    };
+    return blogs.map((blog) => {
+        if (blog.published || props.authenticated) {
+            /** 
+             * Generate the list of blogs. 
+             * Published blogs ar visiable to all
+             * authenticated user can see both published and drafts
+             */
+            return (
+                <Blog key={blog.slug}
+                      title={blog.title}
+                      slug={blog.slug}
+                      body={blog.body}
+                      published={blog.published}
+                      authenticated={props.authenticated}
+                      category={blog.category}
+                      tags={blog.tags}
+                      cut={100}
+                      link={`/blog/${blog.slug}`}/>
+            )
+        } else {
+            return (
+                <Blog key={blog.slug}
+                title={blog.title}
+                slug={blog.slug}
+                body={blog.body}
+                category={blog.category}
+                tags={blog.tags}
+                link={`/blog/${blog.slug}`}/>
+            );
+        }
+    });
+}
+
+function Blogs()  {   
+    return (
+        <div>
+            {blogsData()}
+        </div>
+    ); 
 }
 
 function mapStateToProps(state) {
