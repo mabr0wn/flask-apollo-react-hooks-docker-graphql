d6c1696defd76ca9a15986ab6a85581a4e773226
Matt Brown <mattd429@gmail.com>
2018-11-09 14:01:45 -0500

    commited actions for blogs.js

--- modules/client/src/actions/blogs.js
+++ modules/client/src/actions/blogs.js
@@ -10,39 +10,17 @@ import {
 	FETCH_BLOG,
 	CREATE_BLOG,
 	DELETE_BLOG,
-	UPDATE_BLOG,
-	FETCH_CATEGORIES,
-	FETCH_SETTINGS,
-	CREATE_SUBSCRIBER
+	UPDATE_BLOG
 } from './types';
 
 
-const host = window.location.host.split(':')[0];
-export const ROOT_URL = 'http://api.' + host + '/api/v1';
+export const SERVER_URL = 'http://localhost:4000';
 
-export function fetchBlogs(filter) {
-    var blogs_url = `${ROOT_URL}/blogs/`;
-    var page_url = "";
-    if (filter) {
-	if (filter.currentPage) {
-	    page_url = "?page=" + filter.currentPage;
-	}
-	if (filter.category) {
-	    /* blogs filtered by category */
-	    blogs_url = `${ROOT_URL}/category/${filter.category}`
-	}
-	if (filter.tag) {
-	    /* blogs filtered by tag */
-	    blogs_url = `${ROOT_URL}/tag/${filter.tag}`
-	}
-    }
-    const url = blogs_url + page_url;
-    /* console.log("Fetching blogs"); */
-    return function(dispatch) {
-	axios.get(url)
+export const fetchBlogs = () => {
+    const blogs_url = `${SERVER_URL}/blogs/`;
+    return (dispatch) => {
+	axios.get(blogs_url)
 	     .then(response => {
-		 /* console.log(">>>> src/actions/index.js (promise):");*/
-		 /* console.log("Successfully fetched blogs.Dispatching action FETCH_BLOGS");*/
 		 dispatch({
 		     type: FETCH_BLOGS,
 		     payload: response
@@ -51,16 +29,10 @@ export function fetchBlogs(filter) {
     };
 }
 
-export function fetchBlog(slug) {
-    /* console.log(">>>> src/actions/index.js:");
-     * console.log("Fetching blog.");	    */
-    
-    return function(dispatch) {    
-	axios.get(`${ROOT_URL}/blog/${slug}/`)
-	     .then(response => {
-		 /* console.log("Successfully fetched blog.");
-		    console.log(response.data.body);*/
-		 
+export const fetchBlog = (slug) => {
+    return (dispatch) => {    
+	axios.get(`${SERVER_URL}/blog/${slug}/`)
+	     .then(response => {		 
 		 dispatch({
 		     type: FETCH_BLOG,
 		     payload: response
@@ -70,17 +42,16 @@ export function fetchBlog(slug) {
 }
 
 
-export function createBlog(props) {
+export const createBlog = (props) => {
     // Get the saved token from local storage
     const config = {
 	headers:  { authorization: 'Token ' + localStorage.getItem('token')}
     };
 
-    return function(dispatch) {
-	axios.blog(`${ROOT_URL}/blog/new`, props, config)
+    return (dispatch) => {
+	axios.blog(`${SERVER_URL}/blog/new`, props, config)
 	     .then(response => {
 		 Router.push('/');
-		 /* console.log(response);*/
 		 dispatch({
 		     type: CREATE_BLOG,
 		     payload: response
@@ -90,24 +61,15 @@ export function createBlog(props) {
 }
 
 
-export function updateBlog(slug, blog) {
-    /* console.log(">>>> src/actions/index.js:");
-     * console.log("Getting a token from localStorage. ");	    */
-
+export const updateBlog = (slug, blog) => {
     /* Get the saved token from local storage */
     const config = {
 	headers:  { authorization: 'Token ' + localStorage.getItem('token')}
     };
-
-    /* console.log("Blog Tags: " + blog.tags);*/
-
-    return function(dispatch) {
-	axios.put(`${ROOT_URL}/blog/${slug}/`, blog, config)
+    return (dispatch) => {
+	axios.put(`${SERVER_URL}/blog/${slug}/`, blog, config)
 	     .then(response => {
-		 /* console.log(">>>> src/actions/index.js (promise):");
-		 console.log("Updated a blog. Redirecting to it.");  */
 		 Router.push('/blog/' + response.data.slug);
-		 /* console.log(response);*/
 		 dispatch({
 		     type: UPDATE_BLOG,
 		     payload: response
@@ -116,76 +78,19 @@ export function updateBlog(slug, blog) {
     }
 }
 
-export function deleteBlog(slug) {
-    /* console.log(">>>> src/actions/index.js:");
-     * console.log("Deleting blog.");	    */
-
+export const deleteBlog = (slug) => {
     const config = {
 	headers:  { authorization: 'Token ' + localStorage.getItem('token')}
     };
     
-    return function(dispatch) {    
-	axios.delete(`${ROOT_URL}/blog/${slug}/`, config)
+    return (dispatch) => {    
+	axios.delete(`${SERVER_URL}/blog/${slug}/`, config)
 	     .then(response => {
-		 console.log(">>>> src/actions/index.js (promise):");
-		 console.log("Successfully deleted blog. Dispatching action DELETE_BLOG.");
 		 Router.push('/');		 
 		 dispatch({
 		     type: DELETE_BLOG,
 		     payload: response
 		 });
 	     });
-    };
-    
-}
-
-
-
-export function fetchCategories() {
-    return function(dispatch) {    
-	axios.get(`${ROOT_URL}/categories/`)
-	     .then(response => {
-		 /* console.log("Categories fetched: " + response);*/
-		 dispatch({
-		     type: FETCH_CATEGORIES,
-		     payload: response
-		 });
-	     });
-    };
-}
-
-export function fetchSettings() {
-    return function(dispatch) {    
-	axios.get(`${ROOT_URL}/settings/`)
-	     .then(response => {
-		 /* console.log("Settings fetched: " + JSON.stringify(response));*/
-		 dispatch({
-		     type: FETCH_SETTINGS,
-		     payload: response
-		 });
-	     });
-    };
-}
-
-
-export function createSubscriber(props) {
-    return function(dispatch) {
-	axios.blog(`${ROOT_URL}/subscribe`, props)
-	     .then(response => {
-		 /* browserHistory.push('/');*/
-		 /* console.log(response);*/
-		 dispatch({
-		     type: CREATE_SUBSCRIBER,
-		     payload: response
-		 });
-	     });
-    }
-}
-
-
-export function subscribedClose() {
-    return {
-	type: 'SUBSCRIBED_CLOSE',
-	payload: false
-    }
+    };   
 }
--- modules/client/src/containers/blog/New.jsx
+++ modules/client/src/containers/blog/New.jsx
@@ -1,4 +1,7 @@
-import React, { useState } from 'react'
+import React, { 
+  useState 
+} from 'react'
+
 
 
 function New() {
