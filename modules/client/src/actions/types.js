77c46b3dc888715b5345ab30a6b1cd8091352de2
Matt Brown <mattd429@gmail.com>
2018-11-09 14:38:28 -0500

    updated types

--- modules/client/src/actions/types.js
+++ modules/client/src/actions/types.js
@@ -15,12 +15,42 @@
  * for more practical tips on keeping your codebase clean.
  * 
  */
-// Blog Types
+//BLOG list
 export const LOAD_BLOGS = 'LOAD_BLOGS';
-export const LOAD_BLOG = 'LOAD_BLOG';
+export const LOAD_BLOGS_SUCCESS = 'LOAD_BLOGS_SUCCESS';
+export const LOAD_BLOGS_FAILURE = 'LOAD_BLOGS_FAILURE';
+export const RESET_BLOGS = 'RESET_BLOGS';
+
+//Create new BLOG
 export const CREATE_BLOG = 'CREATE_BLOG';
-export const DELETE_BLOG = 'DELETE_BLOG';
+export const CREATE_BLOG_SUCCESS = 'CREATE_BLOG_SUCCESS';
+export const CREATE_BLOG_FAILURE = 'CREATE_BLOG_FAILURE';
+export const RESET_NEW_BLOG = 'RESET_NEW_BLOG';
+
+// Update BLOG
 export const UPDATE_BLOG = 'UPDATE_BLOG';
+export const UPDATE_BLOG_SUCCESS = 'UPDATE_BLOG_SUCCESS';
+export const UPDATE_BLOG_FAILURE = 'UPDATE_BLOG_FAILURE';
+export const RESET_UPDATE_BLOG = 'RESET_UPDATE_BLOG';
+
+//Validate BLOG fields like Title, Categries on the server
+export const VALIDATE_BLOG_FIELDS = 'VALIDATE_BLOG_FIELDS';
+export const VALIDATE_BLOG_FIELDS_SUCCESS = 'VALIDATE_BLOG_FIELDS_SUCCESS';
+export const VALIDATE_BLOG_FIELDS_FAILURE = 'VALIDATE_BLOG_FIELDS_FAILURE';
+export const RESET_BLOG_FIELDS = 'RESET_BLOG_FIELDS';
+
+//LOAD BLOG
+export const LOAD_BLOG = 'LOAD_BLOG';
+export const LOAD_BLOG_SUCCESS = 'LOAD_BLOG_SUCCESS';
+export const LOAD_BLOG_FAILURE = 'LOAD_BLOG_FAILURE';
+export const RESET_ACTIVE_BLOG = 'RESET_ACTIVE_BLOG';
+
+//Delete BLOG
+export const DELETE_BLOG = 'DELETE_BLOG';
+export const DELETE_BLOG_SUCCESS = 'DELETE_BLOG_SUCCESS';
+export const DELETE_BLOG_FAILURE = 'DELETE_BLOG_FAILURE';
+export const RESET_DELETED_BLOG = 'RESET_DELETED_BLOG';
+
 // Auth Types
 export const AUTH_USER = 'AUTH_USER';
 export const NONAUTH_USER = 'NONAUTH_USER';
