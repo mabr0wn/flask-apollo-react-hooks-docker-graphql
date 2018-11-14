0c4c6a925b51f743246250f01de0291806fde348
Matt Brown <mattd429@gmail.com>
2018-11-14 13:33:53 -0500

    corrected GraphQL fixture blogs.

--- modules/client/src/containers/blog/Blogs.jsx
+++ modules/client/src/containers/blog/Blogs.jsx
@@ -51,10 +51,30 @@ const Blogs = () => {
               if (loading) return <div>Loading...</div>
               if (error) return <div>THERE WAS AN ERROR</div>
               console.log(data)
-              return <div className="blog-post columns-2">
-                    {data.users.edges[0].node.blog.text}
-                    {data.users.edges[0].node.username}
-                    </div>
+              return (
+              <ul className="blog-post columns-2">
+                <li>
+                    <h3>{data.users.edges[0].node.blog.title}</h3>
+                    <p>{data.users.edges[0].node.blog.text}</p>
+                    <p>{data.users.edges[0].node.username}</p>
+                </li>
+                <li>
+                    <h3>{data.users.edges[1].node.blog.title}</h3>
+                    <p>{data.users.edges[1].node.blog.text}</p>
+                    <p>{data.users.edges[1].node.username}</p>
+                </li>
+                <li>
+                    <h3>{data.users.edges[2].node.blog.title}</h3>
+                    <p>{data.users.edges[2].node.blog.text}</p>
+                    <p>{data.users.edges[2].node.username}</p>
+                </li>
+                <li>
+                    <h3>{data.users.edges[3].node.blog.title}</h3>
+                    <p>{data.users.edges[3].node.blog.text}</p>
+                    <p>{data.users.edges[3].node.username}</p>
+                </li>
+              </ul>
+              )
             }}
             </Query>
         </div>
