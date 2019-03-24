8e38f05ad3e9ed5ac635795aabd95506cbe9af28
Matt Brown <mattd429@gmail.com>
2019-03-23 21:48:34 -0400

    testing snapshots

--- modules/client/src/tests/About.test.js
+++ modules/client/src/tests/About.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+
 import About from '../components/core/About.jsx';
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<About />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    const tree = renderer.create(<About />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
